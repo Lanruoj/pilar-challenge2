c6b3371f8a1d6ace7c2c813414ccc3aa320a134b
Tane Kaio <tanesamuelkaio@gmail.com>
2023-04-12 11:07:37 +1000

    Configure `mochawesome`

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      reportFilename: "[status]_[datetime]-[name]-report",
+      timestamp: "longDate",
+      html: false,
+      json: true,
+      overwrite: false,
+      charts: true,
+    },
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
