c93b85779ff81bc62a8a67fd0c132127d8127b3d
Tane Kaio <tanesamuelkaio@gmail.com>
2023-04-27 10:59:27 +1000

    Add doc comments for datepicker commands

--- cypress/support/commands/datepicker.ts
+++ cypress/support/commands/datepicker.ts
@@ -3,11 +3,34 @@ import { getMonthDifference } from "../utils";
 declare global {
   namespace Cypress {
     interface Chainable {
+      /**
+       * Select date in datepicker and verify date has been selected.
+       * @param dateToSelect - date string in format <mm-dd-yyyy>
+       */
       selectDateAndVerify(dateToSelect: string): Chainable<void>;
+      /**
+       * Navigate through date picker by clicking left and right arrows.
+       * @param monthDiff - number of months between current date & target date
+       */
       navigateMonths(monthDiff: number): Chainable<void>;
+      /**
+       * Click on datepicker textbox.
+       */
       clickDatepicker(): Chainable<void>;
+      /**
+       * Verify the year currently selected in the datepicker calendar matches input.
+       * @param date - Date object to be verified
+       */
       verifySelectedYear(date: Date): Chainable<void>;
+      /**
+       * Click the day on the datepicker calendar as specified in the target date input.
+       * @param targetDate - Date object to be parsed
+       */
       clickOnTargetDay(targetDate: Date): Chainable<void>;
+      /**
+       * Verify that the datepicker textbox placeholder is displaying specified date.
+       * @param dateToSelect - date string in format <mm-dd-yyyy>
+       */
       verifyDatepickerPlaceholder(dateToSelect: string): Chainable<void>;
     }
   }
