7abbb84ec279dd3e73195fb0ccdc46af3d41445a
Tane Kaio <tanesamuelkaio@gmail.com>
2023-04-27 10:51:47 +1000

    Refactor into TypeScript

---
+++ cypress/support/commands/datepicker.ts
@@ -0,0 +1,70 @@
+import { getMonthDifference } from "../utils";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      selectDateAndVerify(dateToSelect: string): Chainable<void>;
+      navigateMonths(monthDiff: number): Chainable<void>;
+      clickDatepicker(): Chainable<void>;
+      verifySelectedYear(date: Date): Chainable<void>;
+      clickOnTargetDay(targetDate: Date): Chainable<void>;
+      verifyDatepickerPlaceholder(dateToSelect: string): Chainable<void>;
+    }
+  }
+}
+
+const monthArrowNextButton = ".datepicker-months th.next";
+const monthArrowPrevButton = ".datepicker-months th.prev";
+const selectedDateHeader = ".datepicker-switch";
+const activeDays = ".datepicker-days .day:not(.old)";
+const placeHolderDate = "input.form-control";
+const datepicker = "#datepicker";
+
+Cypress.Commands.add("selectDateAndVerify", (dateToSelect: string) => {
+  // Set up date variables
+  const targetDate = new Date(Date.parse(dateToSelect));
+  const currentDate = new Date();
+  const monthDiff = getMonthDifference(currentDate, targetDate);
+  // Verify datepicker is visible & click
+  cy.clickDatepicker();
+  // Select current selected date
+  cy.verifySelectedYear(currentDate);
+  // If target date is outside of selected month, navigate through months
+  // (can be forwards or backwards)
+  cy.navigateMonths(monthDiff);
+  // Verify selected year matches target date
+  cy.verifySelectedYear(targetDate);
+  // Select target day & click
+  cy.clickOnTargetDay(targetDate);
+  // Verify that placeholder value is target date
+  cy.verifyDatepickerPlaceholder(dateToSelect);
+});
+
+Cypress.Commands.add("navigateMonths", (monthDiff) => {
+  const iterations = new Array(Math.abs(monthDiff));
+  cy.wrap(iterations).each(() => {
+    if (monthDiff > 0) {
+      cy.get(monthArrowNextButton).click({ force: true });
+    } else {
+      cy.get(monthArrowPrevButton).click({ force: true });
+    }
+  });
+});
+
+Cypress.Commands.add("clickDatepicker", () => {
+  cy.get(datepicker).should("be.visible").click();
+});
+
+Cypress.Commands.add("verifySelectedYear", (date) => {
+  const year = date.getFullYear();
+  cy.get(selectedDateHeader).should("contain", year);
+});
+
+Cypress.Commands.add("clickOnTargetDay", (targetDate) => {
+  const day = targetDate.getDate().toString();
+  cy.get(activeDays).contains(day).click();
+});
+
+Cypress.Commands.add("verifyDatepickerPlaceholder", (dateToSelect) => {
+  cy.get(placeHolderDate).invoke("prop", "value").should("equal", dateToSelect);
+});
